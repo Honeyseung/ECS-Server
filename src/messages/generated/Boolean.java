ac0fa7dfdb4fe82d881977610bfcb77a2c02c718
Honeyseung <91928536+Honeyseung@users.noreply.github.com>
2023-03-29 09:59:02 +0900

    Add files via upload

---
+++ src/messages/generated/Boolean.java
@@ -0,0 +1,95 @@
+//
+// 이 파일은 JAXB(JavaTM Architecture for XML Binding) 참조 구현 2.2.8-b130911.1802 버전을 통해 생성되었습니다. 
+// <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a>를 참조하십시오. 
+// 이 파일을 수정하면 소스 스키마를 재컴파일할 때 수정 사항이 손실됩니다. 
+// 생성 날짜: 2022.09.23 시간 08:25:44 AM KST 
+//
+
+
+package generated;
+
+import java.math.BigInteger;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>anonymous complex type에 대한 Java 클래스입니다.
+ * 
+ * <p>다음 스키마 단편이 이 클래스에 포함되는 필요한 콘텐츠를 지정합니다.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;attribute name="mem" use="required" type="{http://www.w3.org/2001/XMLSchema}integer" />
+ *       &lt;attribute name="name" use="required" type="{http://www.w3.org/2001/XMLSchema}integer" />
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
+@XmlRootElement(name = "boolean")
+public class Boolean {
+
+    @XmlAttribute(name = "mem", required = true)
+    protected BigInteger mem;
+    @XmlAttribute(name = "name", required = true)
+    protected BigInteger name;
+
+    /**
+     * mem 속성의 값을 가져옵니다.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigInteger }
+     *     
+     */
+    public BigInteger getMem() {
+        return mem;
+    }
+
+    /**
+     * mem 속성의 값을 설정합니다.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigInteger }
+     *     
+     */
+    public void setMem(BigInteger value) {
+        this.mem = value;
+    }
+
+    /**
+     * name 속성의 값을 가져옵니다.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigInteger }
+     *     
+     */
+    public BigInteger getName() {
+        return name;
+    }
+
+    /**
+     * name 속성의 값을 설정합니다.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigInteger }
+     *     
+     */
+    public void setName(BigInteger value) {
+        this.name = value;
+    }
+
+}
---
+++ src/messages/generated/ObjectFactory.java
@@ -0,0 +1,103 @@
+//
+// 이 파일은 JAXB(JavaTM Architecture for XML Binding) 참조 구현 2.2.8-b130911.1802 버전을 통해 생성되었습니다. 
+// <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a>를 참조하십시오. 
+// 이 파일을 수정하면 소스 스키마를 재컴파일할 때 수정 사항이 손실됩니다. 
+// 생성 날짜: 2022.09.23 시간 08:25:44 AM KST 
+//
+
+
+package generated;
+
+import java.math.BigInteger;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the generated package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _Identifier_QNAME = new QName("", "identifier");
+    private final static QName _Quantity_QNAME = new QName("", "quantity");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: generated
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Boolean }
+     * 
+     */
+    public Boolean createBoolean() {
+        return new Boolean();
+    }
+
+    /**
+     * Create an instance of {@link Data }
+     * 
+     */
+    public Data createData() {
+        return new Data();
+    }
+
+    /**
+     * Create an instance of {@link Float }
+     * 
+     */
+    public Float createFloat() {
+        return new Float();
+    }
+
+    /**
+     * Create an instance of {@link Byte }
+     * 
+     */
+    public Byte createByte() {
+        return new Byte();
+    }
+
+    /**
+     * Create an instance of {@link Message }
+     * 
+     */
+    public Message createMessage() {
+        return new Message();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link BigInteger }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "", name = "identifier")
+    public JAXBElement<BigInteger> createIdentifier(BigInteger value) {
+        return new JAXBElement<BigInteger>(_Identifier_QNAME, BigInteger.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link BigInteger }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "", name = "quantity")
+    public JAXBElement<BigInteger> createQuantity(BigInteger value) {
+        return new JAXBElement<BigInteger>(_Quantity_QNAME, BigInteger.class, null, value);
+    }
+
+}
